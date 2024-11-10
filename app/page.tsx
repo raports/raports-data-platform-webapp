a9db6d1711dade5bc53f085b3949af98c51ee875
Ramis Khasianov <ramis.khasianov@gmail.com>
2024-11-10 17:02:58 +0500

    eslint and prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => (
-  <div className="text-3xl text-blue-700 font-black">Welcome to the app</div>
+  <div className="text-3xl font-black text-blue-700">Welcome to the app</div>
 );
 
 export default Home;
