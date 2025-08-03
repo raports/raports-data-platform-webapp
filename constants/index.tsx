f2f3ffdd0dfcd4c17ca460f10996190e1afa6bf9
ramis-khasianov <ramis.khasianov@gmail.com>
2025-08-03 15:16:41 +0500

    updated packages and moved domain

--- constants/index.tsx
+++ constants/index.tsx
@@ -107,7 +107,7 @@ export const tools = [
     title: "Open Metadata",
     description:
       "Centralized metadata platform to catalog, govern, and manage Raports data ecosystem. Enables data discovery, lineage tracking, and quality monitoring. Deployed on my private Kubernetes cluster.",
-    url: "https://openmetadata.raports.net",
+    url: "https://openmetadata.raports.io",
     image: "/images/tools/openmetadata.png",
     visitButtonText: "Go to Open Metadata",
     tags: ["Data Governance"],
@@ -127,7 +127,7 @@ export const tools = [
     title: "Superset",
     description:
       "Open-source data exploration and visualization platform. Ideal for creating interactive dashboards and running SQL queries against various data sources. I'm using Preset.io's Superset cloud service.",
-    url: "https://4100ea0a.us1a.app.preset.io/superset/welcome/?pageIndex=0&sortColumn=changed_on_delta_humanized&sortOrder=desc",
+    url: "https://superset.raports.io",
     image: "/images/tools/superset.png",
     visitButtonText: "Go to Preset",
     tags: ["BI"],
@@ -137,7 +137,7 @@ export const tools = [
     title: "Airflow",
     description:
       "Workflow orchestration tool that enables the creation, scheduling, and monitoring of data pipelines. Built around Python for flexible pipeline logic. Deployed on my private Kubernetes cluster.",
-    url: "https://airflow.raports.net",
+    url: "https://airflow.raports.io",
     image: "/images/tools/airflow.png",
     visitButtonText: "Go to Airflow",
     tags: ["ETL"],
@@ -147,7 +147,7 @@ export const tools = [
     title: "Airbyte",
     description:
       "Open-source data integration platform that supports syncing data from APIs and databases into warehouses. Extensible and well-suited for Extract and Load workflows. Deployed on my private Kubernetes cluster.",
-    url: "https://airbyte.raports.net",
+    url: "https://airbyte.raports.io",
     image: "/images/tools/airbyte.png",
     visitButtonText: "Go to Airbyte",
     tags: ["ETL"],
@@ -167,7 +167,7 @@ export const tools = [
     title: "Kafka",
     description:
       "Distributed event streaming platform used for real-time data pipelines and stream processing. Supports high-throughput, fault-tolerant messaging between services. Deployed on my private Kubernetes cluster. Access using Conduktor.",
-    url: "https://conduktor.raports.net",
+    url: "https://conduktor.raports.io",
     image: "/images/tools/kafka.png",
     visitButtonText: "Go to Conduktor",
     tags: ["Messaging"],
@@ -177,7 +177,7 @@ export const tools = [
     title: "Spark",
     description:
       "Unified analytics engine for big data processing, with built-in modules for SQL, streaming, machine learning, and graph processing. Deployed on my private Kubernetes cluster",
-    url: "https://spark.raports.net",
+    url: "https://spark.raports.io",
     image: "/images/tools/spark.png",
     visitButtonText: "Go to Spark",
     tags: ["ETL"],
@@ -187,27 +187,17 @@ export const tools = [
     title: "MinIO",
     description:
       "High-performance object storage solution compatible with Amazon S3. Ideal for storing unstructured data like files, backups, and logs. Deployed on my private Kubernetes cluster.",
-    url: "https://minio.raports.net",
+    url: "https://minio.raports.io",
     image: "/images/tools/minio.png",
     visitButtonText: "Go to Minio",
     tags: ["Storage", "S3"],
-  },
-  {
-    id: 10,
-    title: "Elasticsearch",
-    description:
-      "Distributed search and analytics engine designed for horizontal scalability, reliability, and real-time search capabilities. Ideal for log and event data analysis. Deployed on my private Kubernetes cluster.",
-    url: "https://kibana.raports.net",
-    image: "/images/tools/elasticsearch.png",
-    visitButtonText: "Go to Kibana",
-    tags: ["Search", "Analytics"],
   },
   {
     id: 11,
     title: "PostgreSQL",
     description:
       "Advanced open-source relational database with robust SQL support. Suitable for transactional and analytical workloads with strong community backing. Deployed on my private Kubernetes cluster. Access using pgAdmin.",
-    url: "https://pgadmin.raports.net",
+    url: "https://pgadmin.raports.io",
     image: "/images/tools/postgres.png",
     visitButtonText: "Go to pgAdmin",
     tags: ["Database", "SQL"],
@@ -217,7 +207,7 @@ export const tools = [
     title: "Clickhouse",
     description:
       "Column-oriented database management system optimized for OLAP workloads. Offers lightning-fast query performance on large-scale datasets. Deployed on my private Kubernetes cluster. Access using clickhouse's playground.",
-    url: "https://clickhouse.raports.net/play",
+    url: "https://clickhouse.raports.io/play",
     image: "/images/tools/clickhouse.png",
     visitButtonText: "Go to Clickhouse",
     tags: ["Database", "SQL"],
@@ -227,7 +217,7 @@ export const tools = [
     title: "NocoDB",
     description:
       "Turns any SQL database into a smart spreadsheet-like interface. Helps teams collaborate on data without needing to write queries. Deployed on my private Kubernetes cluster.",
-    url: "https://nocodb.raports.net",
+    url: "https://nocodb.raports.io",
     image: "/images/tools/nocodb.png",
     visitButtonText: "Go to NocoDB",
     tags: ["Editor"],
